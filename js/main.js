1aa4524ab2f01c979b32c9cd76b2d4ff238afa6b
Marta Kulig <bialka104b@interia.pl>
2020-08-31 21:06:22 +0200

    209.Adds function draw_Together(), Added patches to _base.scss  and _table.scss

--- js/main.js
+++ js/main.js
@@ -29,37 +29,39 @@ const valuesContainer = document.querySelector(".app__values");
 
 const desktopViev = window.matchMedia("screen and (min-width:500px)");
 
-
+//funkcja łącząca obie dunkcjonalności draw_Mobile_Values() i draw_Mobile_Values()
+const draw_Together = (isDesktop) => {
+  if (isDesktop) {
+    draw_Desktop_Values();
+  } else {
+    draw_Mobile_Values();
+  }
+};
 
 //funkcja będzie renderować wartości odżycze na mobilkach, w postaci listy
 const draw_Mobile_Values = () => {
-    valuesContainer.innerHTML = ""; //czyszczenie elementu
-    let ul = document.createElement('ul');
+  valuesContainer.innerHTML = ""; //czyszczenie elementu
+  let ul = document.createElement("ul");
 
-    values.forEach(spr => {
-        let li = document.createElement("li");
+  values.forEach((spr) => {
+    let li = document.createElement("li");
 
-        let div_name = document.createElement('div');
-        div_name.innerHTML = `Name: ${spr.name}`;
-        let div_calories = document.createElement('div');
-        div_calories.innerHTML = `Calories: ${spr.calories}`;
-        let div_fat = document.createElement('div');
-        div_fat.innerHTML = `Fat: ${spr.fat}`;
-        let div_carbs = document.createElement('div');
-        div_carbs.innerHTML = `Carb: ${spr.carbs}`;
+    let div_name = document.createElement("div");
+    div_name.innerHTML = `Name: ${spr.name}`;
+    let div_calories = document.createElement("div");
+    div_calories.innerHTML = `Calories: ${spr.calories}`;
+    let div_fat = document.createElement("div");
+    div_fat.innerHTML = `Fat: ${spr.fat}`;
+    let div_carbs = document.createElement("div");
+    div_carbs.innerHTML = `Carb: ${spr.carbs}`;
 
-        li.append(div_name, div_calories,div_fat, div_carbs);
+    li.append(div_name, div_calories, div_fat, div_carbs);
 
-        ul.appendChild(li);
-    })
-    
+    ul.appendChild(li);
+  });
 
-
-    valuesContainer.appendChild(ul);
-}
-
-draw_Mobile_Values();
-
+  valuesContainer.appendChild(ul);
+};
 
 // funkcja będzie renderować wartości odżywcze w wersji na komputer, postać tablicy
 const draw_Desktop_Values = () => {
@@ -82,4 +84,20 @@ const draw_Desktop_Values = () => {
   valuesContainer.appendChild(table); //wkładamy table do .app__values
 };
 
-//draw_Desktop_Values();
+//maches nam określa czy warunek który jest przypięty do mojego obiektu desktopViev określa czy to media query jest spełnione
+draw_Together(desktopViev.matches);
+
+desktopViev.addListener((isDesktop) => {
+  //funkcja do tego aby drawTogeter() wywoływał się automatycznie w zależności od szerokości
+  //funkcja działa jak eventlistener tylko nie trzeba podawać argumentu zdarzenia
+  draw_Together(isDesktop.matches);
+});
+
+// alternatywa dla desktopViev.addListener((isDesktop)=>
+// window.addEventListener("resize", () => {
+//   if (window.innerWidth < 500) {
+//     draw_Mobile_Values();
+//   } else {
+//     draw_Desktop_Values();
+//   }
+// });
